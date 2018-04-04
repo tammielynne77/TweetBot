7efa09eae585fa1bbadea5b1adf4a09aeab364e0
TamaraDaniels <34406116+tammielynne77@users.noreply.github.com>
2018-04-03 21:17:38 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+weather_key = 'd06c1e20e4b6d55560cf67fc7c099ba1'
+
+consumer_key = 'Klpz0xbNi0ndOpFuRpvVfX0jh'
+consumer_secret = 'PnRcDQ2drk2xBwciysChhWd9hUrR85R2RiD4cuWWk87FAOx3RQ'
+access_token = '979488724391284737-o6xIqSJbP5MrRmWz4sOm4nrFUiWTnnD'
+access_token_secret = 'FCn3lS67ERP7vuFwb38HJu18lNfPOuKjbqjMeyg8WNOWY'
